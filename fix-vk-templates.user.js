7d8a378ce4ad7fd70f3616727f3a99a4e8b5532f
mcmimik <mcmimik@ya.ru>
2018-12-27 23:20:56 +0300

    Changed version to 0.2

--- fix-vk-templates.user.js
+++ fix-vk-templates.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fix for Шаблоны сообщений ВКонтакте
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Temporary fix for renamed global variable (__stm was renamed to stManager)
 // @author       mcmimik
 // @include      https://vk.com/*
